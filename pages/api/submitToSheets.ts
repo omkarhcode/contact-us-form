f5522ae54b07aedf9f781f602ac3a76b0e5b1085
omkarhcode <omkarh.code@gmail.com>
2023-06-13 19:21:22 +0530

    updated private key to replace /n

--- pages/api/submitToSheets.ts
+++ pages/api/submitToSheets.ts
@@ -24,10 +24,10 @@ export default async function handler(
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY?.split(String.raw`\n`).join(
-          "\n"
-        ),
-        // private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        // private_key: process.env.GOOGLE_PRIVATE_KEY?.split(String.raw`\n`).join(
+        //   "\n"
+        // ),
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       },
       scopes: [
         "https://www.googleapis.com/auth/drive",
