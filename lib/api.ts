79ac0ffefd42c59cfa525e74ed603748633b4d36
omkarhcode <omkarh.code@gmail.com>
2023-06-13 23:53:13 +0530

    commented sendContactFormEmail api code

--- lib/api.ts
+++ lib/api.ts
@@ -1,15 +1,15 @@
-export const sendContactForm = async (data: any) =>
-  fetch("/api/contact", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  }).then((res) => {
-    if (!res.ok) throw new Error("Failed to send message");
-    return res.json();
-  });
+// export const sendContactForm = async (data: any) =>
+//   fetch("/api/contact", {
+//     method: "POST",
+//     body: JSON.stringify(data),
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//   }).then((res) => {
+//     if (!res.ok) throw new Error("Failed to send message");
+//     return res.json();
+//   });
 
 export const sendFormDataToSheet = async (data: any) =>
   fetch("/api/sheets", {
