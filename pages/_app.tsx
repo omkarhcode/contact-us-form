4046a5a8ab2aff7162833cc00c4663614a89d911
omkarhcode <omkarh.code@gmail.com>
2023-06-16 16:09:50 +0530

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import "@/styles/globals.css";
 import theme from "@/theme/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS={true} theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
